79c86de2a649314471b1f47141d7b61061b3254c
Fabio Albertin <fabio.albertin@matchesfashion.com>
2018-05-24 19:03:33 +0100

    Added babel and presets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,4 +2,10 @@ function printme() {
   return 'Hello hot stuff!';
 }
 
+const tmpl = React.createElement('h1', { id: 'title' }, 'Indecision');
+
 console.log(printme());
+ReactDOM.render(
+  tmpl,
+  document.getElementById('app'),
+);
