ada0c55d9d8ee752799a732f224ad37d8b1d1673
Fabio Albertin <albertin@gmail.com>
2018-05-25 22:59:19 +0100

    Finished lecture 11

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,52 @@ function printme() {
   return 'Hello hot stuff!';
 }
 
+var app = {
+  title: 'Indecision',
+  subtitle: 'Not King Crimson'
+};
+
 var tmpl = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  )
+);
+
+var tmpl2vars = {
+  name: 'F. M. Albertin',
+  age: 'Not old',
+  location: 'UK'
+};
+
+var tmpl2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    tmpl2vars.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    tmpl2vars.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    tmpl2vars.location
+  )
 );
 
 console.log(printme());
--- src/app.js
+++ src/app.js
@@ -2,7 +2,31 @@ function printme() {
   return 'Hello hot stuff!';
 }
 
-const tmpl = <h1>Indecision</h1>;
+const app = {
+  title: 'Indecision',
+  subtitle: 'Not King Crimson',
+};
+
+const tmpl = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+const tmpl2vars = {
+  name: 'F. M. Albertin',
+  age: 'Not old',
+  location: 'UK',
+};
+
+const tmpl2 = (
+  <div>
+    <h1>{tmpl2vars.name}</h1>
+    <p>Age: {tmpl2vars.age}</p>
+    <p>Location: {tmpl2vars.location}</p>
+  </div>
+);
 
 console.log(printme());
 ReactDOM.render(
