491b13c3238b49de0fbe65f0baf60c3423780ce3
Fabio Albertin <fabio.albertin@matchesfashion.com>
2018-06-01 18:55:09 +0100

    Finished lecture 25

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -7,10 +7,44 @@ class Person {
     return `Hi ${this.name}`;
   }
   get description() {
-    return `${this.name} is ${this.age} year(s) old.`;
+    return `${this.name} is ${this.age} year(s) old`;
   }
 }
 
 const me = new Person('Michael Dover', 42);
 
 console.log(me.description);
+
+class Student extends Person {
+  constructor(name, age, major = 'CS') {
+    super(name, age);
+    this.major = major;
+  }
+  getMajor() {
+    return !!this.major;
+  }
+  get description() {
+    return `${super.description}${this.getMajor ? ` with major ${this.major}` : ''}.`;
+  }
+}
+
+const her = new Student('hey', 22, 'comms');
+
+console.log(her.description);
+
+class Tourist extends Person {
+  constructor(name, age, hometown = 'NYC') {
+    super(name, age);
+    this.hometown = hometown;
+  }
+  getHometown() {
+    return !!this.hometown;
+  }
+  get greeting() {
+    return `${super.greeting}${this.getHometown ? ` from ${this.hometown}` : ''}.`;
+  }
+}
+
+const them = new Tourist('wonderboy', 34, 'CHI');
+
+console.log(them.greeting);
