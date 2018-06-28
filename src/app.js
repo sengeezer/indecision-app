f80d150317e5f5a88964b8f7a41756ae691cd33e
Fabio Albertin <albertin@gmail.com>
2018-06-28 13:02:10 +0100

    Finished lecture 66

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css/normalize.css';
+
 import IndecisionApp from './components/IndecisionApp';
+
 import './styles/styles.scss';
 
 
